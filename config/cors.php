1647a11a8511e10bf050fe53341ce3b3cba5fb7c
Kenan Fintelman <kenanfintelman123@hotmail.com>
2022-08-17 04:48:47 -0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://escola-api-weld.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
